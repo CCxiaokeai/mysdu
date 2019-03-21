0e3a6d1ea240849594f8cd07d3f7f13980940f85
CC <646521810@qq.com>
2019-03-21 15:41:58 +0800

    first commit

---
+++ DAY 01/base_type.py
@@ -0,0 +1,70 @@
+# 注释
+# model 模块
+# main 主要的
+# print 打印
+# def 声明方法
+# int 整数
+# demo 例子
+# class 类型
+# str string 字符
+# 合法标识符 必须以字母或下划线开头 剩下的可以是字母数字，下划线，大小写敏感，不可用关键字做标识符
+
+# 声明一个int_demo 方法
+def int_demo():
+    # 声明aint变量，并赋值1
+    aint = 1
+    #
+    print(aint)
+    print(type(aint))
+
+
+#
+def str_demo():
+    astr = '1'
+    print(astr)
+    print(type(astr))
+
+
+def str_demo():
+    aint = '1'
+    print(aint)
+    print(type(aint))
+
+
+# 声明一个 float_demo 方法
+def float_demo():
+    # 声明afloat 变量，并赋值 1.90
+    afloat = 1.90
+    # 打印afloat的值
+    print(afloat)
+    # 打印afloat 的类型， type(afloat)，获取afloat的类型
+    print(type(afloat))
+
+
+def add(aint, bint):
+    print(aint)
+    print(bint)
+    return aint + bint
+
+
+# 声明一个sub方法 参数有两个 aint,bint
+def sub(aint, bint):
+    print(aint)
+    print(bint)
+    return aint - bint;
+
+
+if __name__ == '__main__':
+# float_demo()
+
+# int_demo()
+# sub(1,3)
+# print(b)
+
+# a = add(1, 2)
+# print(a)
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+    pass
